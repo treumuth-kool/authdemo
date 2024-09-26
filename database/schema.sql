8f19d825ab97b05755ba16ed9c7f7e00512c100a
treumuth-kool <margus.treumuth@voco.ee>
2024-09-26 15:43:27 +0300

    Add database schema

---
+++ database/schema.sql
@@ -0,0 +1,10 @@
+-- logintest.users definition
+
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
